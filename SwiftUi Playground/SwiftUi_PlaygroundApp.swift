3865f66bf427fe3f9bf55084f63cbcec25f8edfc
yuppe <yuppe.jp@gmail.com>
2022-05-16 14:43:05 +0900

    Initial Commit

---
+++ SwiftUi Playground/SwiftUi_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_PlaygroundApp.swift
+//  SwiftUi Playground
+//
+//  Created by yukio on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
